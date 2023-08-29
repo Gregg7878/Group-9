80fb3b7f8712dfbb1c0e54f3817203afc80b9666
Gregg7878 <theheartbreakkidd777@gmail.com>
2023-08-29 23:12:39 +0300

    finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Event.create([
+  { title: 'Conference', date: Date.today + 1, location: 'Convention Center' },
+  { title: 'Workshop', date: Date.today + 2, location: 'Office Building' },
+  { title: 'Party', date: Date.today + 3, location: '123 Main St' }
+])
+
+puts "Seeded events"
